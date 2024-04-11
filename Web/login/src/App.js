53102bc83384ad6cf173bce00d85ed1d7b643fe4
SandeepaDilshanAlagiyawanna <sandeepa.d.alagiyawanna@gmail.com>
2024-04-11 16:54:21 +0530

    Name of the tab is made

--- Web/login/src/App.js
+++ Web/login/src/App.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f857af03e54bb11873e69d52b93dca64236df10bcb95299dc3b156c69da0c7f1
-size 435
+oid sha256:fce3b5f319eebf785a7058a632618d77df1756eaf4fcf3866ac7d3130406124d
+size 855
